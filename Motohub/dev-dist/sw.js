b76e617ff0d25a1fa0749be95b00666e5d8313dc
Dreev <hed-aabellera@smu.edu.ph>
2025-09-27 16:56:10 +0800

    sw

--- Motohub/dev-dist/sw.js
+++ Motohub/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uua4mj558hg"
+    "revision": "0.0mp1fuetd18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
