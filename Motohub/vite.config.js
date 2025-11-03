99fc58d1959dd031fcced510bd46464556a8295c
Dreev <hed-aabellera@smu.edu.ph>
2025-11-03 17:55:58 +0800

    fix: Add maximum file size limit for caching in Workbox configuration

--- Motohub/vite.config.js
+++ Motohub/vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
